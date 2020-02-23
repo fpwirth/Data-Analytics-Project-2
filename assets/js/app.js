01d6b3ba839ccbac6f735cef93ab6b4a343582db
dowripple <53706893+dowripple@users.noreply.github.com>
2020-02-23 00:51:57 -0600

    jquery and better html!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 // drop down select
-var year_select = d3.select('#year_select');
 
-// loop through and append the year to the drop down
-for (var i=1990; i < 2019; ++i) {
-
-    // append an option to the drop down list for each year
-    var option = year_select.append('option').text(i);
-
-}
+$( "#year_slider" ).slider({
+    orientation: "vertical",
+    range: "min",
+    min: 1990,
+    max: 2018,
+    value: 2018,
+    slide: function( event, ui ) {
+      $( "#year_selected" ).val( ui.value );
+    }
+  });
+  $( "#year_selected" ).val( $( "#year_slider" ).slider( "value" ) );
