c11366d2ac6ff2acbbe3976ee4f65a6162d81beb
dowripple <53706893+dowripple@users.noreply.github.com>
2020-02-23 01:50:25 -0600

    cleaning up js

--- energy_emissions/assets/js/app.js
+++ energy_emissions/assets/js/app.js
@@ -1,13 +1,52 @@
-// drop down select
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    // slider for year
 
-$( "#year_slider" ).slider({
-    orientation: "vertical",
-    range: "min",
-    min: 1990,
-    max: 2018,
-    value: 2018,
-    slide: function( event, ui ) {
-      $( "#year_selected" ).val( ui.value );
-    }
-  });
-  $( "#year_selected" ).val( $( "#year_slider" ).slider( "value" ) );
+    // input to display year on top
+    var $year_selected = $('#year_selected');
+
+    var $year_slider = $("#year_slider" ).slider({
+        orientation: "vertical",
+        range: "min",
+        min: 1990,
+        max: 2018,
+        value: 2018,
+        slide: function( event, ui ) {
+        $( "#year_selected" ).val( ui.value );
+        }
+    });
+    $year_selected.val( $year_slider.slider( "value" ) );
+
+    // dialog for system messages
+    var $sysmessage_dialog = $('#sysmessage_dialog').dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "alert",
+        buttons: [
+            {
+            text: "Ok",
+            class: "ui-button ui-widget ui-corner-all",
+            click: function() {
+                $sysmessage_dialog.dialog('close');
+            }
+            }
+        ]
+
+    });
+
+    // span for message text
+    var $message_text = $('#message_text');
+
+    // filter reset button (sets options back to default)
+    var $filter_reset = $('#filter_reset').button().click(function(d) {
+
+    // set the message an open the dialog
+    $message_text.text('Under construction, not ready yet!')
+
+    // open the dialog
+    $sysmessage_dialog.dialog('open');
+
+
+    })
+    
+});
