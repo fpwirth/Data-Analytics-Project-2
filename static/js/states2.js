e6252843a70d34f763530d806bc4445296e7f1d6
FP Wirth <56011287+fpwirth@users.noreply.github.com>
2020-02-26 16:41:41 -0600

    misc minor edits

--- static/js/states2.js
+++ static/js/states2.js
@@ -8,6 +8,7 @@ d3.json('static/data/state_data.json').then(function(states){
 function buildplot(state,filteredstate){
   var years=filteredstate.map(year=>year.year);
   var greenhousevalues=filteredstate.map(year=>year.greenhouse_emissions);
+  var co2values=filteredstate.map(year=>year.co2_mt_total);
   var ngvalues=filteredstate.map(year=>year.generation_mwh_ng);
   var coalvalues=filteredstate.map(year=>year.generation_mwh_coal);
   var petrovalues=filteredstate.map(year=>year.generation_mwh_petro);
@@ -22,30 +23,29 @@ function buildplot(state,filteredstate){
   var otherbiomassvalues=filteredstate.map(year=>year.generation_mwh_other_biomass);
   var pumpedvalues=filteredstate.map(year=>year.generation_mwh_pumped);
   var greenvalues=filteredstate.map(year=>year.generation_mwh_solar+year.generation_mwh_hydro+year.generation_mwh_wind+year.generation_mwh_nuclear+year.generation_mwh_pumped);
-  console.log(greenvalues);
   var traces=[
-    {x:years,y:coalvalues,name:'Coal',stackgroup:'one',groupnorm:'percent'},
-    {x:years,y:ngvalues,name:'Natural Gas',stackgroup:'one'},
-    {x:years,y:petrovalues,name:'Petroleum',stackgroup:'one'},
-    {x:years,y:othergasvalues,name:'Other Gases',stackgroup:'one'},
-    {x:years,y:othervalues,name:'Other',stackgroup:'one'},
-    {x:years,y:nuclearvalues,name:'Nuclear',stackgroup:'one'},
-    {x:years,y:hydrovalues,name:'Hydro',stackgroup:'one'},
-    {x:years,y:solarvalues,name:'Solar',stackgroup:'one'},
-    {x:years,y:windvalues,name:'Wind',stackgroup:'one'},
-    {x:years,y:geothermalvalues,name:'Geothermal',stackgroup:'one'},
-    {x:years,y:woodvalues,name:'Wood',stackgroup:'one'},
-    {x:years,y:otherbiomassvalues,name:'Other Biomass',stackgroup:'one'},
-    {x:years,y:pumpedvalues,name:'Pumped Storage',stackgroup:'one'},
+    {x:years,y:coalvalues,name:'Coal',stackgroup:'one',groupnorm:'percent',line:{width:.5}},
+    {x:years,y:petrovalues,name:'Petroleum',stackgroup:'one',line:{width:.5}},
+    {x:years,y:ngvalues,name:'Natural Gas',stackgroup:'one',line:{width:.5}},
+    {x:years,y:othergasvalues,name:'Other Gases',stackgroup:'one',line:{width:.5}},
+    {x:years,y:othervalues,name:'Other',stackgroup:'one',line:{width:.5}},
+    {x:years,y:geothermalvalues,name:'Geothermal',stackgroup:'one',line:{width:.5}},
+    {x:years,y:woodvalues,name:'Wood',stackgroup:'one',line:{width:.5}},
+    {x:years,y:otherbiomassvalues,name:'Other Biomass',stackgroup:'one',line:{width:.5}},
+    // {x:years,y:nuclearvalues,name:'Nuclear',stackgroup:'one'},
+    // {x:years,y:hydrovalues,name:'Hydro',stackgroup:'one'},
+    // {x:years,y:solarvalues,name:'Solar',stackgroup:'one'},
+    // {x:years,y:windvalues,name:'Wind',stackgroup:'one'},
+    // {x:years,y:pumpedvalues,name:'Pumped Storage',stackgroup:'one'},
+    {x:years,y:greenvalues,name:'No Emission Sources',stackgroup:'one',line:{width:.5}},
+    {x:years,y:co2values,name:'CO2 Emissions',yaxis:'y2',type:'scatter'},
     {x:years,y:greenhousevalues,name:'Greenhouse Emissions',yaxis:'y2',type:'scatter'}];
   var layout={
     title: `${state} Power Generation by Type`,
     yaxis:{title: 'Generation (MWh)'},
     yaxis2:{title: 'Greenhouse Emissions (metric tons of carbon dioxide)',overlaying:'y',side:'right'},
     legend:{font:{size:4},orientation:'h'}};
-
-    Plotly.newPlot('chart2',traces,layout);
-  };
+  Plotly.newPlot('chart2',traces,layout);};
 
 // function optionChanged(){
 //     let state=d3.select('#year_selected').node().value;
