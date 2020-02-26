cadf8a4dfe8e118142d54084083e114e1c6ce152
FP Wirth <56011287+fpwirth@users.noreply.github.com>
2020-02-25 21:52:33 -0600

    initial stacked area plot

--- energy_emissions/static/js/States.js
+++ energy_emissions/static/js/States.js
@@ -1,18 +1,39 @@
 d3.json('static/js/state_records_test.json').then(function(states){
-  var statedata=states;
-  var state='TX';
-  buildplot(statedata,state);});
+  var statesdata=states;
+  console.log(statesdata);
+  var filteredstate=statesdata.filter(statesdata=>statesdata.state=='TX');
+  console.log(filteredstate);
+  buildplot(filteredstate);
+});
+// function filterCities(city) {
+//   return city.Increase_from_2016 > "15000";
+// }
 
-function buildplot(statedata,state){
-    var filteredstate=statedata.filter(statedata=>statedata.state==state);
-    console.log(filteredstate);
+function buildplot(filteredstate){
+  var years=filteredstate.map(year=>year.year);
+  var ngvalues=filteredstate.map(year=>year.generation_mwh_ng);
+  var coalvalues=filteredstate.map(year=>year.generation_mwh_coal);
+  console.log(years);
+    var traces=[
+      {x:years,y:coalvalues,stackgroup:'one'},
+      {x:years,y:ngvalues,stackgroup:'one'}
+      // {x:filteredstate.year,y:filteredstate.generation_mwh_ng,stackgroup:'one'},
+      // {x:filteredstate.year,y:filteredstate.generation_mwh_petro,stackgroup:'one'},
+      // {x:filteredstate.year,y:filteredstate.generation_mwh_other,stackgroup:'one'}
+    ];
+
+    Plotly.newPlot('chart2', traces, {title: 'Normalized stacked and filled line chart'});
 
     // PLOT STUFF HERE
-};
+  };
 
-function optionChanged(){
-    let state=d3.select('#year_slider').node().value;
-    statedata=statedata;
-    console.log(state);
+// function optionChanged(){
+//     let state=d3.select('#year_selected').node().value;
+//     statedata=statedata;
+//     console.log(state);
     // buildplot(statedata,state);
-};
+//};
+
+// function filterstates(states) {
+//   return states.state==state;
+// }
